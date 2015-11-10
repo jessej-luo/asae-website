ce600e9ab0c9da96858b8d0ccb355f3fedf6da61
Jesse Luo <jessej.luo@berkeley.edu>
2015-11-09 21:53:16 -0800

    Added Sponsors and such, need to finish editing Team

--- asae-2/assets/javascript/script.js
+++ asae-2/assets/javascript/script.js
@@ -1,51 +1,7 @@
-(function($) {
-    $('.picture').each( function() {
-        // Get the items.
-        var $pic     = $(this),
-            getItems = function() {
-                var items = [];
-                $pic.find('figure').each(function() {
-                    var $href    = $(this).find('a').attr('href'),
-                        $size    = $(this).find('a').data('size').split('x'),
-                        $width   = $size[0],
-                        $height  = $size[1],
-                        $caption = $(this).find('figcaption').html();
-
-                    var item = {
-                        src   : $href,
-                        w     : $width,
-                        h     : $height,
-                        title : $caption
-                    }
-
-                    items.push(item);
-                });
-                return items;
-            }
-
-        var items = getItems();
-
-        // Preload image.
-        var image = [];
-        $.each(items, function(index, value) {
-            image[index]     = new Image();
-            image[index].src = value['src'];
-        });
-
-        // Binding click event.
-        var $pswp = $('.pswp')[0];
-        $pic.on('click', 'figure', function(event) {
-            event.preventDefault();
-            
-            var $index = $(this).index();
-            var options = {
-                index: $index,
-                bgOpacity: 0.7,
-                showHideOpacity: true
-            }
-
-            var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-            lightBox.init();
-        });
-    });
-})(jQuery);
+$('.thumbnail').click(function(){
+    $('.modal-body').empty();
+    var title = $(this).parent('a').attr("title");
+    $('.modal-title').html(title);
+    $($(this).parents('div').html()).appendTo('.modal-body');
+    $('#myModal').modal({show:true});
+});
